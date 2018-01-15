b874e0d65b7eeb1905992b66f85fbebf6b44de28
Alexandre Borela <alexandre.borela@gmail.com>
2018-01-15 13:02:12 -0200

    Make linter happy

--- src/__tests__/resolveView.js
+++ src/__tests__/resolveView.js
@@ -8,9 +8,7 @@
 // distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 
-import { defaultView } from '..'
-import { presentable } from '..'
-import { resolveView } from '..'
+import { defaultView, presentable, resolveView } from '..'
 import { Component } from 'react'
 
 describe('method “resolveView”', () => {
